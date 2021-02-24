386b57295cb4e7b835da1320b5c96ee230e43158
Alex Montoya <NeuroPyrox@Gmail.com>
2021-02-24 03:21:55 -0700

    Write parser grammar

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -2,10 +2,11 @@
 (require br-parser-tools/lex)
 (require (prefix-in : br-parser-tools/lex-sre))
 (require brag/support)
+; Pay close attention to whitespace in this file cause it has 3 different definitions
 (define-lex-abbrevs
-  (alphanum (:/ "0" "9" "a" "f" "A" "F"))
+  (alphanum (:/ "0" "9" "a" "z" "A" "Z"))
   (digit (:/ "0" "9"))
-  (non-ascii (:/ #\u0 #\u9f))
+  (non-ascii (:~ (:/ #\u0 #\u9f)))
   (new-line (:or "\n" "\r\n" "\r" "\f"))
   (whitespace (:* (char-set " \t\r\n\f"))) ; Shadows the pre-existing whitespace macro
   (unicode (:: #\\
@@ -23,10 +24,10 @@
   (ident (:: (:? "-")
              name-start
              (:* name-char)))
-  (num (:or (:+ digit)
-            (:: (:* digit)
-                "."
-                (:+ digit))))
+  (number (:or (:+ digit)
+               (:: (:* digit)
+                   "."
+                   (:+ digit))))
   ; Many of the following rules could be made DRYer, but imo that would obscure them
   (double-quote-string (:: #\"
                            (:* (:or (:~ (char-set "\n\r\f\\\""))
@@ -73,10 +74,54 @@
   (bad-uri-1 (:: uri-start string whitespace))
   (bad-uri-2 (:: uri-start bad-string))
   (bad-uri (:or bad-uri-0 bad-uri-1 bad-uri-2)))
+; TODO attach lexemes
 (define css-lexer
   (lexer
-   [whitespace 1]
-   [any-char 2]))
+   [ident 'ident]
+   [(:: "@" ident) 'at-keyword]
+   [string 'string]
+   [bad-string 'bad-string]
+   [bad-uri 'bad-uri]
+   [bad-comment 'bad-comment]
+   [(:: "#" name) 'hash]
+   [number 'number]
+   [(:: number "%") 'percentage]
+   [(:: number ident) 'dimension]
+   ; TODO review uri syntax
+   [(:or
+     (:: uri-start whitespace string whitespace ")")
+     (:: uri-start
+         (:* (:or (char-set "!#$%&*-~")
+                  non-ascii escape))
+         whitespace ")"))
+    'uri]
+   [(:: "u+"
+        (:** 1 6 (:or alphanum "?"))
+        (:? (:: "-" (:** 1 6 alphanum))))
+    'unicode-range]
+   ["<!--" 'cdo]
+   ["-->" 'cdc]
+   [":" 'colon]
+   [";" 'semicolon]
+   ["{" 'open-curly-bracket]
+   ["}" 'close-curly-bracket]
+   ["(" 'open-parentheses]
+   [")" 'close-parentheses]
+   ["[" 'open-square-bracket]
+   ["]" 'close-square-bracket]
+   [(:+ (char-set " \t\r\n\f")) 'whitespace] ; Different from the whitespace macro
+   [(:: "/*"
+        (:* (:~ "*"))
+        (:+ "*")
+        (:* (:: (:~ (char-set "/*"))
+                (:* (:~ "*"))
+                (:+ "*")))
+        "/")
+    'comment]
+   [(:: ident "(") 'function]
+   ["~=" 'includes]
+   ["|=" 'dash-match]
+   [any-char 'delim]))
 (define (make-tokenizer port)
   (thunk (css-lexer port)))
 (define (test x) (apply-tokenizer-maker make-tokenizer x))
